23f467da6af5b56cf52540d2177b4e40a476ffd7
SamSchultz-bit <130870042+SamSchultz-bit@users.noreply.github.com>
2024-02-22 15:02:25 -0600

    update content

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,27 +3,19 @@ import React from 'react';
 function Intro() {
    return (
       <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
-         <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">Forrest</h1>
-         <p className="text-base md:text-xl mb-3 font-medium">Software Engineer & Web Developer</p>
-         <p className="text-sm max-w-xl mb-6 font-bold">
-            I'm an indie full-stack developer and content creator building my
-            version of the digital world one step at a time. All coding projects
-            are built from the ground up, from planning and designing all the way
-            to solving real-life problems with code.
-            <br />
-            All video content is built the same way, from ideation and planning,
-            all the way to finalizing the content with artistic touches. I publish
-            that content on my YouTube channel{' '}
-            <a
-               href="https://youtube.com/fknight"
-               target="_blank"
-               className="text-cyan-600 hover:underline underline-offset-2 decoration-2 decoration-red-600"
-               rel="noreferrer noopener"
-            >
-               ForrestKnight
-            </a>{' '}
-            to more than 400k subscribers.
-         </p>
+         <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">Samuel Schultz</h1>
+         <p className="text-base md:text-xl mb-3 font-medium">Data Analyst</p>
+         <p className="text-sm max-w-xl mb-2 font-bold">
+         Leveraging expertise in SQL, Tableau, and Python, I extract actionable insights from complex data, fueling strategic decision-making with clear narratives.
+        </p>
+        <p className="text-sm max-w-xl mb-2 font-bold">
+            I am a skilled problem-solver with strong communication, eager to contribute to a full-time data analyst role where I can:
+            <ul>
+                <li className="text-sm max-w-xl mt-2 mb-2 font-bold">Optimize decision making through data-driven solution</li>
+                <li className="text-sm max-w-xl mb-2 font-bold">Partner with cross-functional teams for impactful strategies.</li>
+                <li className="text-sm max-w-xl mb-6 font-bold">Continuously learn and explore cutting-edge data trends.</li>
+            </ul>
+        </p>
       </div>
    )
 }
