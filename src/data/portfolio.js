79992a305952bbdec8add51f075a5d4a87394547
SamSchultz-bit <130870042+SamSchultz-bit@users.noreply.github.com>
2024-02-22 13:42:11 -0600

    Initial Commit

---
+++ src/data/portfolio.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        title: 'EX1 ',
+        imgUrl: '',
+        stack: ['test', 'test'],
+        link: 'https://fkcodes.com',
+    },
+    {
+        title: 'This Website',
+        imgUrl: '',
+        stack: ['React', 'Tailwind', 'Vite'],
+        link: 'https://thehackershideout.com',
+    },
+    {
+        title: 'EX2 ',
+        imgUrl: '',
+        stack: ['test', 'test'],
+        link: 'https://fkcodes.com',
+    },
+    {
+        title: 'EX3 ',
+        imgUrl: '',
+        stack: ['test', 'test'],
+        link: 'https://fkcodes.com',
+    },
+];
---
+++ src/data/timeline.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        year: '2024',
+        title: 'Test',
+        duration: '2 years',
+        details: 'ipsem lorem de facto jslij ipsem lorem de facto jslij ipsem lorem de facto jslij ipsem lorem de facto jslij ipsem lorem de facto jslij ipsem lorem de facto jslij ipsem lorem de facto jslij ipsem lorem de facto jslij ',
+    },
+    {
+        year: 'Today',
+        title: 'Title',
+        duration: 'time',
+        details: 'details',
+    },
+    {
+        year: 'Today',
+        title: 'Title',
+        duration: 'time',
+        details: 'details',
+    },
+    {
+        year: 'Today',
+        title: 'Title',
+        duration: 'time',
+        details: 'details',
+    },
+    {
+        year: 'Today',
+        title: 'Title',
+        duration: 'time',
+        details: 'details',
+    }
+]
